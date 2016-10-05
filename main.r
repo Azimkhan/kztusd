bdd7edf31c21ca23a9b9e9ea8317560e5e953b39
azimkhan <wanrossen@gmail.com>
2016-10-05 15:25:38 +0600

    all months added

--- main.r
+++ main.r
@@ -2,8 +2,16 @@ jan <- c(150.81,155.4,184.45)
 feb <- c(150.45,184.06,185.05)
 mar <- c(150.84,182.04,185.65)
 apr <- c(151.24,182.05,185.80)
+may <- c(151.1, 182.02,185.9)
+jun <- c(151.4, 183.32,185.82)
+jul <- c(151.8, 183.32,186.22)
+aug <- c(153.67, 182.55,187.74)
+sep <- c(153.2, 181.9,239)
+oct <- c(153.93, 181.92,271.4)
+nov <- c(154.38,181,278.78)
+dec <- c(154.3,181.524,307.44)
 
-m <- cbind(jan,feb,mar,apr)
+m <- cbind(jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec)
 
 rownames(m) <- c("2013","2014","2015")
 m
@@ -13,7 +21,7 @@ glob_max = max(m)
 
 colors <- c('red','green','blue')
 matplot(t(m),type='l',ylim=c(glob_min-2,glob_max+2), col=colors)
-legend('bottomright', inset=.05, legend=rownames(m), 
+legend('topleft', inset=.05, legend=rownames(m), 
       pch=1, horiz=TRUE, col=colors)
 
 mean_2014 <- mean(m[2,])
